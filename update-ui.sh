bbdc37a45e45ef8e82a45c53227a8794ebb472cc
Cristi Vîjdea <cristi@cvjd.me>
2017-12-28 16:09:56 +0100

    Remove unneeded files from swagger-ui dist

--- update-ui.sh
+++ update-ui.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-set -e
-
+set -ev
 npm update
 cp node_modules/redoc/dist/redoc.min.js src/drf_yasg/static/drf-yasg/redoc/redoc.min.js
 cp -r node_modules/swagger-ui-dist src/drf_yasg/static/drf-yasg/
-rm -f src/drf_yasg/static/drf-yasg/swagger-ui-dist/package.json src/drf_yasg/static/drf-yasg/swagger-ui-dist/.npmignore
+pushd src/drf_yasg/static/drf-yasg/swagger-ui-dist/ >/dev/null
+rm -f package.json .npmignore README.md
+rm -f index.html *.map
+popd >/dev/null
