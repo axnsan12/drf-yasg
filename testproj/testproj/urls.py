27e5525f95fa5324b8663f4710f4e657dfcd5680
Cristi Vîjdea <cristi@cvjd.me>
2018-01-07 10:47:33 +0100

    Add links in API description

--- testproj/testproj/urls.py
+++ testproj/testproj/urls.py
@@ -10,7 +10,11 @@ from drf_yasg.views import get_schema_view
 swagger_info = openapi.Info(
     title="Snippets API",
     default_version='v1',
-    description="Test description",
+    description="""This is a demo project for the [drf-yasg](https://github.com/axnsan12/drf-yasg) Django Rest Framework library.
+
+The `swagger-ui` view can be found [here](/cached/swagger).  
+The `ReDoc` view can be found [here](/cached/redoc).  
+The swagger YAML document can be found [here](/cached/swagger.yaml).  """,
     terms_of_service="https://www.google.com/policies/terms/",
     contact=openapi.Contact(email="contact@snippets.local"),
     license=openapi.License(name="BSD License"),
