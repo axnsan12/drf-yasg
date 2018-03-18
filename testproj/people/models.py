2dcb14c1383e28b358b40dbf9d2bae42deae3b07
Kévin Beaubestre <kevin@beaubestre.fr>
2018-03-18 17:27:06 +0100

    Add: 'people' app that breaks drf_yasg without previous commits

---
+++ testproj/people/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Identity(models.Model):
+    firstName = models.CharField(max_length=30, null=True)
+    lastName = models.CharField(max_length=30, null=True)
+
+
+class Person(models.Model):
+    Identity = models.OneToOneField(Identity, related_name='person')
