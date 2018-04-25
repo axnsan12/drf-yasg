aa90723703e238ea4a0c9fa07ddf760b197455d6
Cristi Vîjdea <cristi@cvjd.me>
2018-04-25 22:55:25 +0300

    Switch to redoc 2.0 alpha
    Update redoc to 2.0.0-alpha.17

--- update-ui.sh
+++ update-ui.sh
@@ -2,7 +2,7 @@
 set -ev
 npm update
 
-cp node_modules/redoc/dist/redoc.min.js src/drf_yasg/static/drf-yasg/redoc/
+cp node_modules/redoc/bundles/redoc.standalone.js src/drf_yasg/static/drf-yasg/redoc/redoc.min.js
 cp -r node_modules/swagger-ui-dist src/drf_yasg/static/drf-yasg/
 pushd src/drf_yasg/static/drf-yasg/swagger-ui-dist/ >/dev/null
 rm -f package.json .npmignore README.md
