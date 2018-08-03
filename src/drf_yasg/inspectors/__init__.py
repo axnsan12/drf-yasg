822f669a58fd04a619b2518e3ed9e34bf9574db5
John Carter <john@therefromhere.org>
2018-08-03 23:16:20 +1200

    fix spelling

--- src/drf_yasg/inspectors/__init__.py
+++ src/drf_yasg/inspectors/__init__.py
@@ -10,7 +10,7 @@ from .field import (
 from .query import CoreAPICompatInspector, DjangoRestResponsePagination
 from .view import SwaggerAutoSchema
 
-# these settings must be accesed only after definig/importing all the classes in this module to avoid ImportErrors
+# these settings must be accessed only after defining/importing all the classes in this module to avoid ImportErrors
 ViewInspector.field_inspectors = swagger_settings.DEFAULT_FIELD_INSPECTORS
 ViewInspector.filter_inspectors = swagger_settings.DEFAULT_FILTER_INSPECTORS
 ViewInspector.paginator_inspectors = swagger_settings.DEFAULT_PAGINATOR_INSPECTORS
