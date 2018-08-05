13eb68ca46ec7edc3a6fe6ae06079aa484ebaee9
John Carter <john@therefromhere.org>
2018-08-05 13:33:02 +1200

    lint

--- testproj/users/serializers.py
+++ testproj/users/serializers.py
@@ -1,4 +1,5 @@
 import sys
+
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
--- testproj/users/serializers_old_py.py
+++ testproj/users/serializers_old_py.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+
 from drf_yasg.utils import swagger_serializer_method
 
 
