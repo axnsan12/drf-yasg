9b2d878f2841156a4612ee4bb2d06e2fa6747948
Kevin Brown <kevin@kevin-brown.com>
2019-09-20 17:06:06 -0400

    Migrate errors to built-in DRF exception handling
    Instead of requiring a middleware to do the rendering, we can instead
    just use the built-in error handling provided by DRF. All this
    required was for us to subclass `APIException` and to set the `detail`
    property on the exception itself.

--- src/drf_yasg/middleware.py
+++ src/drf_yasg/middleware.py
@@ -12,12 +12,4 @@ class SwaggerExceptionMiddleware(object):
         return self.get_response(request)
 
     def process_exception(self, request, exception):
-        if isinstance(exception, SwaggerValidationError):
-            err = {'errors': exception.errors, 'message': str(exception)}
-            codec = exception.source_codec
-            if isinstance(codec, _OpenAPICodec):
-                err = codec.encode_error(err)
-                content_type = codec.media_type
-                return HttpResponse(err, status=500, content_type=content_type)
-
         return None  # pragma: no cover
