1352c2a23b491ca1249f40c2db71588417e6a6eb
Étienne Noss <etienne.noss@wifirst.fr>
2019-09-29 18:01:28 +0200

    fix Optional typing hint for SerializerMethodField (#428)

--- src/drf_yasg/inspectors/field.py
+++ src/drf_yasg/inspectors/field.py
@@ -610,6 +610,8 @@ class SerializerMethodFieldInspector(FieldInspector):
             # look for Python 3.5+ style type hinting of the return value
             hint_class = inspect_signature(method).return_annotation
 
+            if not inspect.isclass(hint_class) and hasattr(hint_class, '__args__'):
+                hint_class = hint_class.__args__[0]
             if inspect.isclass(hint_class) and not issubclass(hint_class, inspect._empty):
                 type_info = get_basic_type_info_from_hint(hint_class)
 
