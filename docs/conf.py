b2ed3f761bda93850a16979950a7f86ec9839749
JoelLefkowitz <45922542+JoelLefkowitz@users.noreply.github.com>
2020-10-12 11:02:55 +0100

    Hotfix/package name typo (#32)
    * Fix package name typo
    
    * Bump version

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ from pkg_resources import get_distribution
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-version = "1.18.0"
+version = "1.18.1"
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 
