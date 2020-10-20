ef4bb4c76b57585f5026bec16e37eec9f347a71a
johnthagen <johnthagen@users.noreply.github.com>
2020-10-20 10:25:57 -0400

    Update bundled swagger-ui and redoc Javascript dependencies (#69)

--- scripts/update-ui.sh
+++ scripts/update-ui.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+
 set -ev
 
-npm update
+# Do a fresh reinstall of packages to ensure they are consistent with
+# package.json/package-lock.json
+npm ci
 
-cp node_modules/redoc/bundles/redoc.standalone.js src/drf_yasg/static/drf_yasg2/redoc/redoc.min.js
-wget https://rebilly.github.io/ReDoc/releases/v1.x.x/redoc.min.js -O src/drf_yasg/static/drf_yasg2/redoc-old/redoc.min.js
+cp node_modules/redoc/bundles/redoc.standalone.js src/drf_yasg2/static/drf_yasg2/redoc/redoc.min.js
+wget https://rebilly.github.io/ReDoc/releases/v1.x.x/redoc.min.js -O src/drf_yasg2/static/drf_yasg2/redoc-old/redoc.min.js
 
-cp -r node_modules/swagger-ui-dist src/drf_yasg/static/drf_yasg2/
-pushd src/drf_yasg/static/drf_yasg2/swagger-ui-dist/ >/dev/null
+cp -r node_modules/swagger-ui-dist src/drf_yasg2/static/drf_yasg2/
+pushd src/drf_yasg2/static/drf_yasg2/swagger-ui-dist/ >/dev/null
 rm -f package.json .npmignore README.md favicon-16x16.png
-rm -f  swagger-ui.js index.html *.map
+rm -f swagger-ui.js index.html *.map
 popd >/dev/null
