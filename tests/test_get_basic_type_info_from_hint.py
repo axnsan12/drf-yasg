03aa1432e920c6401cd60dba28750f8e473be2cf
Cristi Vîjdea <cristi@cvjd.me>
2020-10-25 19:27:06 +0200

    Add support for python 3.9 generic primitives
    Fixes #627, #631

--- tests/test_get_basic_type_info_from_hint.py
+++ tests/test_get_basic_type_info_from_hint.py
@@ -1,9 +1,18 @@
+import sys
+
 import pytest
 import uuid
 from typing import Dict, List, Optional, Set, Union
 
 from drf_yasg import openapi
 from drf_yasg.inspectors.field import get_basic_type_info_from_hint
+
+python39_generics_tests = []
+if sys.version_info >= (3, 9):
+    python39_generics_tests = [
+        (dict[int, int], {'type': openapi.TYPE_OBJECT, 'format': None}),
+        (list[bool], {'type': openapi.TYPE_ARRAY, 'items': openapi.Items(openapi.TYPE_BOOLEAN)}),
+    ]
 
 
 @pytest.mark.parametrize('hint_class, expected_swagger_type_info', [
@@ -13,6 +22,7 @@ from drf_yasg.inspectors.field import get_basic_type_info_from_hint
     (dict, {'type': openapi.TYPE_OBJECT, 'format': None}),
     (Dict[int, int], {'type': openapi.TYPE_OBJECT, 'format': None}),
     (uuid.UUID, {'type': openapi.TYPE_STRING, 'format': openapi.FORMAT_UUID}),
+    (list, {'type': openapi.TYPE_ARRAY, 'items': openapi.Items(openapi.TYPE_STRING)}),
     (List[int], {'type': openapi.TYPE_ARRAY, 'items': openapi.Items(openapi.TYPE_INTEGER)}),
     (List[str], {'type': openapi.TYPE_ARRAY, 'items': openapi.Items(openapi.TYPE_STRING)}),
     (List[bool], {'type': openapi.TYPE_ARRAY, 'items': openapi.Items(openapi.TYPE_BOOLEAN)}),
@@ -31,7 +41,7 @@ from drf_yasg.inspectors.field import get_basic_type_info_from_hint
     (type('SomeType', (object,), {}), None),
     (None, None),
     (6, None),
-])
+] + python39_generics_tests)
 def test_get_basic_type_info_from_hint(hint_class, expected_swagger_type_info):
     type_info = get_basic_type_info_from_hint(hint_class)
     assert type_info == expected_swagger_type_info
