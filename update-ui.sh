68ce45c19a622bd759ec1e28d700c5c6e484f992
Cristi Vîjdea <cristi@cvjd.me>
2021-12-12 13:46:24 +0200

    Copy redoc LICENSE files
    ref #756

--- update-ui.sh
+++ update-ui.sh
@@ -3,7 +3,9 @@ set -ev
 npm update
 
 cp node_modules/redoc/bundles/redoc.standalone.js src/drf_yasg/static/drf-yasg/redoc/redoc.min.js
+cp node_modules/redoc/LICENSE src/drf_yasg/static/drf-yasg/redoc/LICENSE
 curl -o src/drf_yasg/static/drf-yasg/redoc-old/redoc.min.js https://rebilly.github.io/ReDoc/releases/v1.x.x/redoc.min.js
+curl -o src/drf_yasg/static/drf-yasg/redoc-old/LICENSE https://raw.githubusercontent.com/Redocly/redoc/v1.x/LICENSE
 
 cp -r node_modules/swagger-ui-dist src/drf_yasg/static/drf-yasg/
 pushd src/drf_yasg/static/drf-yasg/swagger-ui-dist/ >/dev/null
