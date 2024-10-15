ac659ecfa17d82a2a16e603e15ec5a7ec34a8fe0
Joel Lefkowitz <joellefkowitz@hotmail.com>
2024-10-15 17:43:06 +0100

    Remove twine-check and ignore some flake8 errors (#895)
    * Remove twine-check and ignore some flake8 errors
    
    * Add flake8 ignore tags to tox sections
    
    * Bump twine version

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,6 @@ modindex_common_prefix = ['drf_yasg.']
 # html_theme = 'default'
 
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
