43347701266b271fb44209ca7ef3cd4e5b97bf4f
Nikita Karamov <me@kytta.dev>
2025-04-18 23:52:40 +0200

    Replace pkg_resources with importlib.metadata

--- src/drf_yasg/__init__.py
+++ src/drf_yasg/__init__.py
@@ -5,13 +5,7 @@ __email__ = "cristi@cvjd.me"
 
 try:
     from importlib.metadata import version
-
-    __version__ = version(__name__)
 except ImportError:  # Python < 3.8
-    try:
-        from pkg_resources import DistributionNotFound, get_distribution
+    from importlib_metadata import version
 
-        __version__ = get_distribution(__name__).version
-    except DistributionNotFound:  # pragma: no cover
-        # package is not installed
-        pass
+__version__ = version(__name__)
